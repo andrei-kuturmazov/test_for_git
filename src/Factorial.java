aa6c375681c44dfc3154b0c95cbf6cce85cf09a4
andrei-kuturmazov <andrei.kuturmazov@gmail.com>
2019-08-27 15:07:41 +0300

    Fibonachi

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,13 +1,14 @@
 public class Factorial {
     public static void main(String[] args) {
-        System.out.println(factorial(25));
+        System.out.println(factorial(20));
     }
 
 
     public static long factorial (long a) {
-        if (a == 1) {
-            return 1;
-        }
-        return a * factorial(a - 1);
+            if (a == 1) {
+                return 1;
+            }
+            return a * factorial(a - 1);
+
     }
 }
