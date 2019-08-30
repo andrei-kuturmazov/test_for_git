f535bb3564ccd53e57826e493f5134c483b52919
andrei-kuturmazov <andrei.kuturmazov@gmail.com>
2019-08-30 15:24:33 +0300

    Volatile test

---
+++ src/Volatile.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+import static java.lang.Thread.*;
+
+public class Volatile {
+    public static void main(String[] args) {
+        VolatileTest test = new VolatileTest();
+        test.start();
+
+        Scanner s = new Scanner(System.in);
+        s.nextLine();
+
+        test.shutdown();
+    }
+
+
+}
+
+
+class VolatileTest extends Thread {
+    private volatile boolean running = true;
+
+
+    public void run() {
+        while (running)
+            System.out.println("Hello world here");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void shutdown() {
+        this.running = false;
+
+    }
+}
