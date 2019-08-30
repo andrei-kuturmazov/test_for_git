8fd5c6582467c5e27dd22822464cd89ebf5393e7
andrei-kuturmazov <andrei.kuturmazov@gmail.com>
2019-08-30 14:48:42 +0300

    Thread.sleep and try block

--- src/Multithreading.java
+++ src/Multithreading.java
@@ -1,7 +1,7 @@
 public class Multithreading {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-
+        Thread.sleep(1000);
         MyThread firstThread = new MyThread();
         MyThread secondThread = new MyThread();
         firstThread.start();
@@ -16,6 +16,11 @@ public class Multithreading {
 class MyThread extends Thread {
     @Override
     public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         for (int i =0; i < 1000; i++ ) {
             System.out.println("Hello world from thread, integer value is " + i);
         }
