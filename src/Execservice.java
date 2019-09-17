d141aa463f4d581e2854fe20d410fabf7316bce6
andrei-kuturmazov <andrei.kuturmazov@gmail.com>
2019-09-17 16:42:06 +0300

    executor service test

---
+++ src/Execservice.java
@@ -0,0 +1,40 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Execservice {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 5; i++ )
+            executorService.submit(new Work(i));
+
+
+        executorService.shutdown();
+        System.out.println("All tasks were submitted");
+
+        executorService.awaitTermination(1, TimeUnit.DAYS);
+        }
+    }
+
+
+
+
+    class Work implements Runnable {
+
+    private int id;
+
+    public Work (int id) {
+        this.id = id;
+    }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread number " + id + " finished");
+        }
+    }
